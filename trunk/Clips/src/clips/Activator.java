8471d16efee7d637044c727a7b4192a7c6531fc5
sandipchitale <sandipchitale@31d3bad0-b99f-11dd-9d36-1d0c09c95ed7>
2008-11-27 20:42:16 +0000

    Formatting. Cleanup imports. Cleanup dependencies. Javadoc.

--- trunk/Clips/src/clips/Activator.java
+++ trunk/Clips/src/clips/Activator.java
@@ -8,77 +8,86 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * 
+ * @author Sandip V. Chitale
+ * 
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "Clips";
-	
-	public static final String TEXT = "icons/text.gif";
-	
-	public static final String AUTO_CLIP_CUT_COPY = "autoClipCutAndCopy";
-	public static final String MAX_CLIPS_COUNT = "maxClips";
-	
-	private static final boolean defaultAUTO_CLIP_CUT_COPY = false;
-	private static final int defaultMAX_CLIPS_COUNT = 64;
+    // The plug-in ID
+    public static final String PLUGIN_ID = "Clips";
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-		plugin = this;
-	}
+    public static final String TEXT = "icons/text.gif";
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+    public static final String AUTO_CLIP_CUT_COPY = "autoClipCutAndCopy";
+    public static final String MAX_CLIPS_COUNT = "maxClips";
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    private static final boolean defaultAUTO_CLIP_CUT_COPY = false;
+    private static final int defaultMAX_CLIPS_COUNT = 64;
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-	
+    // The shared instance
+    private static Activator plugin;
+
+    /**
+     * The constructor
+     */
+    public Activator() {
+        plugin = this;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+     * )
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+     * )
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
     @Override
     protected void initializeDefaultPreferences(IPreferenceStore store) {
-	    store.setDefault(AUTO_CLIP_CUT_COPY, defaultAUTO_CLIP_CUT_COPY);
-	    store.setDefault(MAX_CLIPS_COUNT, defaultMAX_CLIPS_COUNT);
+        store.setDefault(AUTO_CLIP_CUT_COPY, defaultAUTO_CLIP_CUT_COPY);
+        store.setDefault(MAX_CLIPS_COUNT, defaultMAX_CLIPS_COUNT);
     }
-	
-	public boolean isAutoClipCutCopy() {
-	    return getPreferenceStore().getBoolean(AUTO_CLIP_CUT_COPY);
-	}
-	
-	public int getMaxClipsCount() {
-	    return getPreferenceStore().getInt(MAX_CLIPS_COUNT);
-	}
-	
-	@Override
-	protected void initializeImageRegistry(ImageRegistry reg) {
-		reg.put(TEXT, createImageDescriptor(TEXT));
-	}
-	
-	private ImageDescriptor createImageDescriptor(String id) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, id);
-	}
+
+    public boolean isAutoClipCutCopy() {
+        return getPreferenceStore().getBoolean(AUTO_CLIP_CUT_COPY);
+    }
+
+    public int getMaxClipsCount() {
+        return getPreferenceStore().getInt(MAX_CLIPS_COUNT);
+    }
+
+    @Override
+    protected void initializeImageRegistry(ImageRegistry reg) {
+        reg.put(TEXT, createImageDescriptor(TEXT));
+    }
+
+    private ImageDescriptor createImageDescriptor(String id) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, id);
+    }
 
 }
--- trunk/Clips/src/clips/views/SwapAction.java
+++ trunk/Clips/src/clips/views/SwapAction.java
@@ -7,16 +7,23 @@ import org.eclipse.ui.IViewPart;
 
 import clips.model.ClipsModel;
 
+/**
+ * This action swaps the top two clips.
+ *
+ * @author Sandip V. Chitale
+ *
+ */
 public class SwapAction implements IViewActionDelegate {
 
-	public void init(IViewPart view) {}
+    public void init(IViewPart view) {
+    }
 
-	public void run(IAction action) {
-		ClipsModel.getINSTANCE().swap();
-	}
+    public void run(IAction action) {
+        ClipsModel.getINSTANCE().swap();
+    }
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		action.setEnabled(ClipsModel.getINSTANCE().get().length > 1);
-	}
+    public void selectionChanged(IAction action, ISelection selection) {
+        action.setEnabled(ClipsModel.getINSTANCE().get().length > 1);
+    }
 
 }
