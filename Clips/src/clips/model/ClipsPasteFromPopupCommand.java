7631a0644a68d876885b2168863602b8eab5d3e9
sandipchitale <sandipchitale@31d3bad0-b99f-11dd-9d36-1d0c09c95ed7>
2009-01-04 04:10:49 +0000

    Use multiline text control to show the selection in view and paste pop up.

--- Clips/src/clips/model/ClipsPasteFromPopupCommand.java
+++ Clips/src/clips/model/ClipsPasteFromPopupCommand.java
@@ -4,10 +4,20 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.window.Window;
+import org.eclipse.osgi.internal.resolver.ComputeNodeOrder;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -50,11 +60,36 @@ public class ClipsPasteFromPopupCommand implements IHandler {
                                 .beep();
                         return null;
                     }
-                    ListDialog listDialog = new ListDialog(activeWorkbenchWindow.getShell());  
+                    ListDialog listDialog = new ListDialog(activeWorkbenchWindow.getShell()) {
+                    	@Override
+                    	protected Control createDialogArea(Composite container) {
+                    		Composite composite = (Composite) super.createDialogArea(container);
+                    		final Text text = new Text(composite, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.READ_ONLY);
+                    		GridData gridData = new GridData(GridData.FILL_BOTH);
+                    		gridData.heightHint = convertHeightInCharsToPixels(6);
+                    		text.setLayoutData(gridData);
+                    		getTableViewer().addSelectionChangedListener(new ISelectionChangedListener() {
+								public void selectionChanged(
+										SelectionChangedEvent event) {
+									ISelection selection = event.getSelection();
+									if (selection instanceof IStructuredSelection) {
+										IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+										if (structuredSelection.size() > 0) {
+											text.setText(structuredSelection.getFirstElement().toString());
+											return;
+										}
+									}
+									text.setText("");
+								}
+                    		});
+							return composite;
+                    	}
+                    };
+                    listDialog.setHelpAvailable(false);
                     listDialog.setAddCancelButton(true);  
                     listDialog.setContentProvider(new ArrayContentProvider());  
                     listDialog.setLabelProvider(new ClipLabelProvider());  
-                    listDialog.setInput(clips);  
+                    listDialog.setInput(clips);
                     listDialog.setTitle("Select a clip to insert");  
                     if (listDialog.open() == Window.OK) {
                     	Object[] results = listDialog.getResult();
